8f53b69a8248687eda3e474d0357407f9bf40a8d
sidney2k1 <sidsingla02@gmail.com>
2025-01-29 17:16:22 +0530

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+def Hanoi(n,a,b,c):
+    if n==1:
+        print("Move disc 1 from rod",a,"to",b)
+        return
+    Hanoi(n-1,a,c,b)
+    print("Move disc",n,"from",a,"to",b)
+    Hanoi(n-1,c,b,a)
+n=2
+Hanoi(n,"A","B","C")
